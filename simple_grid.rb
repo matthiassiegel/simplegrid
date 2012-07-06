ad7e4031692748c74d7c87fc61a4af67f690c7ff
Matthias Siegel <matthiassiegel@gmail.com>
2012-07-06 13:57:11 +0200

    Bugfixes, and exist? method added

--- simple_grid.rb
+++ simple_grid.rb
@@ -15,6 +15,17 @@ class SimpleGrid
   
   
   #
+  # Check if a file exists
+  #
+  # @param [Hash] selector A query selector, e.g. :filename => 'foo.txt', :content_type => 'image/jpg'
+  # @param [Boolean] prefix GridFS collection prefix
+  #
+  def exist?(selector)
+    @@grid.exist?(selector) ? true : false
+  end
+  
+  
+  #
   # Store a new file
   #
   # @param [String, #read] data String or IO-like object
@@ -102,22 +113,23 @@ class SimpleGrid
     if result.count > 1
       result.map! { |r| read(r['_id']) }
     else
-      result.blank? ? [] : [read(result['_id'])]
+      result.blank? ? [] : [read(result.first['_id'])]
     end
   end
   
   
   #
   # Delete a file by its unique ID
-  # Note: in safe mode, Grid.delete doesn't return Boolean, so success is confirmed by calling #find
+  # Note: in safe mode, Grid.delete doesn't return Boolean, so success is confirmed by calling #exist?
   #
   # @param [BSON::ObjectId] ID BSON::ObjectId of the file to delete
   # @return [Boolean] False if the file didn't exist or couldn't be deleted. True otherwise.
   #
   def delete(id)
     begin
+      return false unless exist?(id)
       @@grid.delete(id)
-      find(id) ? false : true
+      exist?(id) ? false : true
     rescue
       false
     end
@@ -132,7 +144,14 @@ class SimpleGrid
   #
   def delete_by_filename(filename)
     result = find_by_filename(filename)
-    result.each { |r| delete(r['_id']) } ? true : false
+    
+    if result.count > 1
+      success = true
+      result.each { |r| success = false unless delete(r['_id']) }
+      success
+    else
+      result.blank? ? false : delete(result.first['_id'])
+    end
   end
   
 end
